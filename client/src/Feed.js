48d8b4d91c52f256b9166a3620897a839e8c5bf4
vagrant <frederique.bordeleau@gmail.com>
2020-03-16 17:58:48 +0000

    Loading clothing pictures on /feed

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -6,15 +6,12 @@ function Feed() {
   const { state, dispatch } = useApplicationData();
 
   const clothingList = state.users.map(clothing => (
-    <li key={clothing.image_url}>
-      {clothing.image_url}
-    </li>
+    <img src={clothing.image_url}></img >
   ));
 
   return (
     <div className='App'>
       <h1>Clothings</h1>
-
       {state.loading && <h3>Loading...</h3>}
 
       <ul>{!state.loading && clothingList}</ul>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
+import Feed from './Feed'
 import './index.scss';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/feed" component={Feed} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
