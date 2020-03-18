b8321f5f4624258420ee1d0844d8a40fe6ec4d28
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 09:49:40 -0400

    implement registration component and connect to backend

---
+++ client/src/components/auth/Registration.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Registration(props) {
+
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password:"",
+    password_confirmation: "",
+    registrationErrors: ""
+  })
+
+  const handleSubmit = (e) => {
+
+    axios.post("http://localhost:3001/registrations", {
+      user: {
+        name: state.name,
+        email: state.email,
+        password: state.password,
+        password_confirmation: state.password_confirmation
+      }
+    },
+      { withCredentials: true }
+    ).then(res => console.log("registration res", res))
+     .catch(err => console.log("registration error", err));
+
+    e.preventDefault();
+  }
+
+  const handleChange = (e) => {
+    setState({...state, 
+      [e.target.name]: e.target.value
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name="name"
+          placeholder="Name"
+          value={state.name}
+          onChange={handleChange}
+          required>
+        </input>
+
+        <input 
+          type="email" 
+          name="email"
+          placeholder="Email Address"
+          value={state.email}
+          onChange={handleChange}
+          required>
+        </input>
+
+        <input 
+          type="password" 
+          name="password"
+          placeholder="Password"
+          value={state.password}
+          onChange={handleChange}
+          required>
+        </input>
+
+        <input 
+          type="password" 
+          name="password_confirmation"
+          placeholder="Password confirmation"
+          value={state.password_confirmation}
+          onChange={handleChange}
+          required>
+        </input>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
