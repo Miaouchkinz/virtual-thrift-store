797812ef8a70bdd578359857d6c3d91c89d983b4
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 15:18:56 -0400

    re-add in the other table urls

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
-  namespace :api do
-    resources :users, 
-              :clothings, 
-              :bookmarks, 
-              :clothing_categories
-  end
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+  resources :users, :clothings, :bookmarks, :clothing_categories
+  root to: "static#home"
 end
