5ba603bab99ebf8faaee9dbd12ebcdf290f92ba1
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 18:19:45 -0400

    namespace registration and table routes to api

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  namespace :api do
+    resources :registrations, only: [:create]
+    resources :users, :clothings, :bookmarks, :clothing_categories
+  end
+  
   resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
-  resources :users, :clothings, :bookmarks, :clothing_categories
+
   root to: "static#home"
 end
