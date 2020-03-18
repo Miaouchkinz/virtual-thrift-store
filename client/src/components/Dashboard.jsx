3198c3587a2a1d70f1af04b854a9ece146122805
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 11:44:09 -0400

    add loggedInStatus state for testing

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function Dashboard(props) {
+export default function Dashboard({ loggedInStatus }) {
 
   return (
     <div>
       <div>
         <h1>Dashboard</h1>
+        <h2>Status: {loggedInStatus}</h2>
       </div>
     </div>
   );
