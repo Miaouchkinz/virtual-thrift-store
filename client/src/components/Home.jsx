309543cfb84f6542673f07221f043e0e5a3959cb
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 14:32:35 -0400

    implement logout call to back end to clear cookie session

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Registration from './auth/Registration';
 import Login from './auth/Login';
+import axios from 'axios';
 
 export default function Home(props) {
-  const { loggedInStatus, history, handleLogin } = props
+  const { loggedInStatus, history, handleLogin, handleLogout } = props
   
   const handleSuccessfulAuth = (data) => {
     handleLogin(data);
     history.push('/dashboard')
+  }
+
+  const handleLogoutClick = () => {
+    axios.delete('http://localhost:3001/logout', {
+      withCredentials: true
+    })
+    .then(res => handleLogout())
+    .catch(err => console.log("logout err", err));
   }
 
   return (
@@ -16,6 +25,7 @@ export default function Home(props) {
       <h2>Status: {loggedInStatus}</h2>
       <Registration handleSuccessfulAuth={handleSuccessfulAuth}/>
       <Login handleSuccessfulAuth={handleSuccessfulAuth}/>
+      <button onClick={() => handleLogoutClick()}>Logout</button>
     </div>
   );
 };
