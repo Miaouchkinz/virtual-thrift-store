0fb6edacee1bc522d7b818ec2f9c64140d4b3827
vagrant <frederique.bordeleau@gmail.com>
2020-03-18 00:07:07 +0000

    Added active key to categories obkect

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -2,33 +2,11 @@ import React, { useState } from 'react';
 import '../Feed.scss'
 
 export default function Feed({ clothing, clothingCategories }) {
-  // clothingCategories GOES ABOVE IN PROPS LATER
 
-  // const clothingCategories = {
-  //   "01": { name: "T-Shirt", id: 1, active: true },
-  //   "02": { name: "Pants", id: 2, active: true }
-  // }
 
-  const categoryList = (allCategories) => {
-    let categoriesResult = [];
-    for (let category in allCategories) {
-      categoriesResult.push(
-        < label key={[category].id} >
-          {allCategories[category].name}
-          < input
-            name={[category].id}
-            type='checkbox'
-          // checked={type[category.name]}
-          // onChange={() => setType({ ...type, tshirt: !type.pants })}
-          />
-        </label >
-      )
-    }
-    return categoriesResult;
-  };
 
-  //1- Test 01 and 02 for rendering below
-  //2- Axios GET all clothing_category
+
+
   //3- Render all the list with all active: true
   //4- Filter everything with .filter in clothingList
 
@@ -37,6 +15,25 @@ export default function Feed({ clothing, clothingCategories }) {
   const [type, setType] = useState(clothingCategories);
 
   // const activeList = Object.values(type).filter(current => current.active).map(current => current.id)
+
+  const categoryList = (allCategories) => {
+    let categoriesResult = [];
+    for (let category in allCategories) {
+      allCategories[category].active = true;
+      categoriesResult.push(
+        < label key={[category].id} >
+          {allCategories[category].name}
+          < input
+            name={[category].id}
+            type='checkbox'
+            checked={type[name]}
+            onChange={() => setType({ ...type, active: !type.active })}
+          />
+        </label >
+      )
+    }
+    return categoriesResult;
+  };
 
 
   const [size, setSize] = useState({
