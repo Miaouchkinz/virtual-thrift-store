be02adf4f11fc02953995f8aecce0fe8d922e28e
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 14:06:34 -0400

    implement login component to save to session cookies when successful

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Login(props) {
+
+  // TOFIX: NEED TO MOVE TO USEAPPDATA
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password:"",
+    LoginErrors: ""
+  })
+
+  const handleSubmit = (e) => {
+    // TOFIX: NEED TO MOVE TO USEAPPDATA
+    axios.post("http://localhost:3001/sessions", {
+      user: {
+        email: state.email,
+        password: state.password,
+      }
+    },
+      { withCredentials: true }
+    ).then(res => {
+      if(res.data.logged_in) {
+        props.handleSuccessfulAuth(res.data)
+      }
+    })
+     .catch(err => console.log("login error", err));
+
+    e.preventDefault();
+  }
+
+  const handleChange = (e) => {
+    setState({...state, 
+      [e.target.name]: e.target.value
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="email" 
+          name="email"
+          placeholder="Email Address"
+          value={state.email}
+          onChange={handleChange}
+          required>
+        </input>
+
+        <input 
+          type="password" 
+          name="password"
+          placeholder="Password"
+          value={state.password}
+          onChange={handleChange}
+          required>
+        </input>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
