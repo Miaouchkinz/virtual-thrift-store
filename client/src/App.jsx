a748b6e8c7911f3873695fbfa7e4b3b84e68f584
Gab Richard <g.elodierichard@gmail.com>
2020-03-19 10:28:42 -0400

    add routing for landing, signup, and landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import Home from './components/Home'
-import Dashboard from './components/Dashboard'
+import Landing from './components/Landing';
+import Dashboard from './components/Dashboard';
+import Login from './components/auth/Login';
+import Registration from './components/auth/Registration';
 import './App.scss';
 import useApplicationData from './hooks/useApplicationData';
 
 export default function App(props) {
  const { 
    state,
-   handleLogin,
+   handleSuccessfulAuth,
    handleLogout
   } = useApplicationData();
 
@@ -24,16 +26,23 @@ export default function App(props) {
             exact 
             path={"/"}
             render={props => (
-              <Home 
+              <Landing 
                 {...props}
-                handleLogin={handleLogin}
+                handleSuccessfulAuth={handleSuccessfulAuth}
                 handleLogout={handleLogout}
-                loggedInStatus={state.loggedInStatus}
               />
             )}>
           </Route>
+          <Route exact path={"/login"} render={(props) => (
+            <Login {...props} handleSuccessfulAuth={handleSuccessfulAuth}/>
+          )}>
+          </Route>
+          <Route exact path={"/register"} render={(props => (
+            <Registration {...props} handleSuccessfulAuth={handleSuccessfulAuth}/>
+          ))}>
+          </Route>
           <Route exact path={"/dashboard"}>
-            <Dashboard loggedInStatus={state.loggedInStatus}/>
+            <Dashboard loggedInStatus={state.loggedInStatus} handleLogout={handleLogout}/>
           </Route>
         </Switch>
       </Router>
