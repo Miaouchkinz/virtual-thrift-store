1fe44f996c2a6153d458c181d07eb82772703a2e
Gab Richard <g.elodierichard@gmail.com>
2020-03-19 10:29:53 -0400

    update name to landing and add in html structure and button components

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './common/button';
+
+export default function Landing(props) {
+  const { history } = props;
+
+  return (
+    <main id="landing">
+      <div className="logo-placeholder"></div>
+      <h1 className="welcome-message">Welcome to TraderJane's</h1>
+      <Button onClick={() => history.push('/login')} label="LOGIN" primary fullWidth />
+      <Button onClick={() => history.push('/register')} label="SIGN UP" secondary fullWidth />
+    </main>
+  );
+};
