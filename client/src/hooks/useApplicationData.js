68d48a42c747fcf1e1ab1f5b0232423669902de5
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 21:03:30 -0400

    implement login state functionality

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,26 +1,65 @@
 import { useEffect, useReducer } from 'react';
-import dataReducer, { SET_USERS } from '../reducers/dataReducer';
+import dataReducer, { SET_USERS, SET_CURRENT_USER } from '../reducers/dataReducer';
 import axios from 'axios';
 
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(dataReducer, {
     users: [],
     loading: true,
+    currentUser: {},
+    loggedInStatus: "NOT_LOGGED_IN"
   });
+
+  const handleLogout = () => {
+    dispatch({
+      type: SET_CURRENT_USER,
+      value: { 
+        currentUser:{},
+        loggedInStatus: "NOT_LOGGED_IN"
+      }});
+  };
+
+  const handleLogin = (data) => {
+    dispatch({
+      type: SET_CURRENT_USER,
+      value: { 
+        currentUser: data.user,
+        loggedInStatus: "LOGGED_IN"
+      }});
+  };
+
   useEffect(() => {
-    axios({
-      method: 'GET',
-      url: 'http://localhost:3001/api/users',
+    Promise.all([
+      axios.get('http://localhost:3001/api/users'),
+      axios.get('http://localhost:3001/logged_in', { withCredentials: true })
+    ])
+    .then((all) => {
+      dispatch({ type: SET_USERS, users: all[0].data });
+
+      // Handle Login status & current user
+      if(all[1].data.logged_in && state.loggedInStatus === "NOT_LOGGED_IN") {
+        dispatch({
+          type: SET_CURRENT_USER,
+          value: { 
+            currentUser: all[1].data.user,
+            loggedInStatus: "LOGGED_IN"
+          }});
+      } else if (!all[1].data.logged_in && state.loggedInStatus === "LOGGED_IN") {
+        dispatch({
+          type: SET_CURRENT_USER,
+          value: { 
+            currentUser:{},
+            loggedInStatus: "NOT_LOGGED_IN"
+          }});
+      }
     })
-      .then(({ data }) => {
-        dispatch({ type: SET_USERS, users: data });
-      })
-      .catch(err => console.log(err));
+    .catch(err => console.log(err));
   }, []);
 
   return {
     state,
-    dispatch,
+    handleLogout,
+    handleLogin
   };
 };
 
