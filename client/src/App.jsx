2addd8ef17eed9f036a3c3faccf83235616eb201
Gab Richard <g.elodierichard@gmail.com>
2020-03-18 21:01:12 -0400

    more state logic and axios calls to useApplicationData

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { 
   BrowserRouter as Router, 
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Home from './components/Home'
 import Dashboard from './components/Dashboard'
 import './App.scss';
 import useApplicationData from './hooks/useApplicationData';
-import axios from 'axios';
 
 export default function App(props) {
- const { state, dispatch } = useApplicationData();
-
- // TOFIX: NEED TO MOVE TO USEAPPDATA
-  const [ userLogInfo, setUserLogInfo ] = useState({
-    loggedInStatus: "NOT_LOGGED_IN",
-    user: {}
-  })
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/logged_in", { withCredentials: true })
-      .then(res => {
-        if(res.data.logged_in && userLogInfo.loggedInStatus === "NOT_LOGGED_IN") {
-          setUserLogInfo({
-            loggedInStatus: "LOGGED_IN",
-            user: res.data.user
-          })
-        } else if (!res.data.logged_in && userLogInfo.loggedInStatus === "LOGGED_IN") {
-          setUserLogInfo({
-            loggedInStatus: "NOT_LOGGED_IN",
-            user: {}
-          })  
-        }
-      })
-      .catch(err => console.log("check login err", err));
-  }, []);
-
-  const handleLogout = () => {
-    setUserLogInfo({
-      loggedInStatus: "NOT_LOGGED_IN",
-      user: {}  
-    })
-  }
-
-  const handleLogin = (data) => {
-    setUserLogInfo({
-      loggedInStatus: "LOGGED_IN",
-      user: data.user
-    })
-  }
+ const { 
+   state,
+   handleLogin,
+   handleLogout
+  } = useApplicationData();
 
   return (
     <div className='App'>
@@ -64,12 +28,12 @@ export default function App(props) {
                 {...props}
                 handleLogin={handleLogin}
                 handleLogout={handleLogout}
-                loggedInStatus={userLogInfo.loggedInStatus}
+                loggedInStatus={state.loggedInStatus}
               />
             )}>
           </Route>
           <Route exact path={"/dashboard"}>
-            <Dashboard loggedInStatus={userLogInfo.loggedInStatus}/>
+            <Dashboard loggedInStatus={state.loggedInStatus}/>
           </Route>
         </Switch>
       </Router>
