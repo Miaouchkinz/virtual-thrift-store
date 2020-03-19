4407d71c45b60f4d69ef00e026bf861c14e84039
Gab Richard <g.elodierichard@gmail.com>
2020-03-19 10:30:26 -0400

    add in logout button for testing while profile component is not built

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
-export default function Dashboard({ loggedInStatus }) {
+export default function Dashboard({ loggedInStatus, handleLogout }) {
+
+  const handleLogoutClick = () => {
+    axios.delete('http://localhost:3001/logout', {
+      withCredentials: true
+    })
+    .then(res => handleLogout())
+    .catch(err => console.log("logout err", err));
+  }
 
   return (
     <div>
       <div>
         <h1>Dashboard</h1>
         <h2>Status: {loggedInStatus}</h2>
+        <button onClick={() => handleLogoutClick()}>Logout</button>
       </div>
     </div>
   );
