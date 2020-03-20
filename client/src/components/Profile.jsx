d37f7a83a572525ae03e12a3e017ad9563dbb9a1
Gab Richard <g.elodierichard@gmail.com>
2020-03-20 17:32:09 -0400

    implement top profile component and navigation for logout and feed

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from './common/textButton';
+import Icon from './common/iconButton';
+import axios from 'axios';
 
 export default function Profile(props) {
+  const {
+    history,
+    userName,
+    avatar,
+    handleLogout
+  } = props;
+
+  // tabs: closet, convos, camera
+  const [tab, setTab] = useState('closet');
+
+  const handleLogoutClick = () => {
+    axios.delete('http://localhost:3001/logout', {
+      withCredentials: true
+    })
+    .then(res => {
+      handleLogout()
+      history.push('/')
+    })
+    .catch(err => console.log("logout err", err));
+  }
+
   return (
-    <h1>This is Profile</h1>
-  )
-}
+    <div id="profile-page">
+      <div id="wave">
+        <div id="inner-wave"></div>
+      </div>
+      <nav>
+        <Icon secondary onClick={() => history.push('/feed')} label="<"></Icon>
+        <Button onClick={() => handleLogoutClick()} primary label="LOG OUT"/>
+      </nav>
+      <header>
+        <img
+          className="avatar_url-image"
+          src={avatar}
+          alt="user_avatar"
+        />
+        <span className="user-name">{userName}</span>
+        <div>
+          <Icon primary large onClick={() => setTab('closet')} selected={tab === 'closet'} icon="hanger"></Icon>
+          <Icon primary large onClick={() => setTab('convos')} selected={tab === 'convos'} icon="convos"></Icon>
+          <Icon primary large onClick={() => setTab('camera')} selected={tab === 'camera'} icon="camera"></Icon>
+        </div>
+      </header>
+      <main>
+        <div className="profile-feed-placeholder"></div>
+      </main>
+    </div>
+  );
+
+};
