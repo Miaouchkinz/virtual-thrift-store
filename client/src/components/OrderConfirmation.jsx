c234b36ab66581abf562e2574d68c7f40260e88e
Fred <frederique.bordeleau@gmail.com>
2020-03-21 18:07:42 -0400

    Added thank you message and earth icon to confirmation

--- client/src/components/OrderConfirmation.jsx
+++ client/src/components/OrderConfirmation.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Icon from "./common/iconButton";
 
 export default function OrderConfirmation(props) {
   return (
-    <h1>This is OrderConfirmation</h1>
-  )
+    <div>
+      <header>
+        <div className="feed_header_block">
+          <Link
+            to={{
+              pathname: "/user/profile"
+            }}
+          >
+            <Icon secondary icon=""></Icon>
+          </Link>
+          <Link
+            to={{
+              pathname: "/feed"
+            }}
+          >
+            <Icon secondary label="Home"></Icon>
+          </Link>
+        </div>
+      </header>
+      <div className="thank_you_message">
+        <img src="./images/earth.png" />
+        <h1>
+          Together, we're <br /> saving the <br /> planet.
+        </h1>
+      </div>
+    </div>
+  );
 }
