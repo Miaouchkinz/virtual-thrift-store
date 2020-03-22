0881160c3390df3acd7a35cf9d7bb74bf1b8ed74
Gab Richard <g.elodierichard@gmail.com>
2020-03-22 17:15:38 -0400

    implement newMessageForm component

---
+++ client/src/components/chat/NewMessageForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+// import axios from "axios";
+import { API_ROOT, HEADERS } from '../../constants';
+
+
+class NewMessageForm extends React.Component {
+  state = {
+    text: '',
+    conversation_id: this.props.conversation_id
+  };
+
+  componentWillReceiveProps = nextProps => {
+    this.setState({ conversation_id: nextProps.conversation_id });
+  };
+
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    fetch(`${API_ROOT}/messages`, {
+      method: 'POST',
+      headers: HEADERS,
+      body: JSON.stringify(this.state)
+    });
+    this.setState({ text: '' });
+  };
+
+  render = () => {
+    return (
+      <div className="newMessageForm">
+        <form onSubmit={this.handleSubmit}>
+          <label>New Message:</label>
+          <br />
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  };
+}
+
+export default NewMessageForm;
