afb642ca0460e4838ad2ec89a02c2165e5be7c09
Gab Richard <g.elodierichard@gmail.com>
2020-03-22 17:16:07 -0400

    add alt attribute to img tag

--- client/src/components/common/iconButton.jsx
+++ client/src/components/common/iconButton.jsx
@@ -18,7 +18,7 @@ export default function Icon(props) {
   if (large) classList.push('large')
   if (selected) classList.push('selected');
 
-  const content = icon ? <img src={`/images/${icon}.png`}/> : label;
+  const content = icon ? <img src={`/images/${icon}.png`} alt={`${icon} button`} /> : label;
 
   return (
     <button className={`icon-btn ${classList.join(' ')}`} type={type} onClick={onClick}>{content}</button>
