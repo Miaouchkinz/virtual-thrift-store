b799bcb11518ac411a26f2c442d5785573134f16
Gab Richard <g.elodierichard@gmail.com>
2020-03-22 17:12:54 -0400

    update conversations and messages table for ease of testing

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_010434) do
+ActiveRecord::Schema.define(version: 2020_03_22_205522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,16 +45,15 @@ ActiveRecord::Schema.define(version: 2020_03_22_010434) do
   create_table "conversations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "messages", force: :cascade do |t|
     t.string "text"
     t.bigint "conversation_id"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "user_conversations", force: :cascade do |t|
@@ -80,7 +79,6 @@ ActiveRecord::Schema.define(version: 2020_03_22_010434) do
   add_foreign_key "clothings", "clothing_categories"
   add_foreign_key "clothings", "users"
   add_foreign_key "messages", "conversations"
-  add_foreign_key "messages", "users"
   add_foreign_key "user_conversations", "conversations"
   add_foreign_key "user_conversations", "users"
 end
