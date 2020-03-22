2aa19d09d428bf9b1f7e8c306371063a4e6b8e54
Gab Richard <g.elodierichard@gmail.com>
2020-03-22 09:58:28 -0400

    add api constants

---
+++ client/src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_ROOT = 'http://localhost:3001';
+export const API_WS_ROOT = 'ws://localhost:3001/api/cable';
+export const HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
