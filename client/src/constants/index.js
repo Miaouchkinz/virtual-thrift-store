37619a6da4ddd2b2f40133aff9623bd0ce749f95
Gab Richard <g.elodierichard@gmail.com>
2020-03-22 17:16:45 -0400

    update api_root route for constant

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://localhost:3001';
+export const API_ROOT = 'http://localhost:3001/api';
 export const API_WS_ROOT = 'ws://localhost:3001/api/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
