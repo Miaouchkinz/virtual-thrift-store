2d76893d7f647c00cc3264d3e508311a94cf9feb
Gab Richard <g.elodierichard@gmail.com>
2020-03-21 23:35:05 -0400

    implemented create method

--- backend/app/controllers/messages_controller.rb
+++ backend/app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+    # Used for saving received data and broadcasting that data to the appropriate channels.
+    def create
+      message = Message.new(message_params)
+      conversation = Conversation.find(message_params[:conversation_id])
+      
+      if message.save
+        serialized_data = ActiveModelSerializers::Adapter::Json.new(
+          MessageSerializer.new(message)
+        ).serializable_hash
+        MessagesChannel.broadcast_to conversation, serialized_data
+        head :ok
+      end
+    end
+    
+    private
+    
+    def message_params
+      params.require(:message).permit(:text, :conversation_id)
+    end
 end
