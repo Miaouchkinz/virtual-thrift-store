ffe5c6d1e65aa64e19e7ae1b5805e910fa9ba6b8
Gab Richard <g.elodierichard@gmail.com>
2020-03-22 10:00:55 -0400

    wrap app in actionCableProvider component and pass down API_WS_ROOT

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <ActionCableProvider url={API_WS_ROOT}>
+    <App />
+  </ActionCableProvider>,
   document.getElementById('root')
 );
