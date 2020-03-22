24a0d1b04b250c8f28739d2538bb575becd6bea6
Gab Richard <g.elodierichard@gmail.com>
2020-03-21 23:34:55 -0400

    implemented index and create methods

--- backend/app/controllers/conversations_controller.rb
+++ backend/app/controllers/conversations_controller.rb
@@ -1,2 +1,28 @@
 class ConversationsController < ApplicationController
+  # Going to be used for the front-end’s initial fetch request to receive the current existing
+  # conversations and their messages.
+  def index
+    conversations = Conversation.all
+    render json: conversations
+  end
+
+  # Used for saving received data and broadcasting that data to the appropriate channels.
+  def create
+    conversation = Conversation.new(conversation_params)
+
+    if conversation.save
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        ConversationSerializer.new(conversation)
+      ).serializable_hash
+      
+      ActionCable.server.broadcast 'conversations_channel', serialized_data
+      head :ok
+    end
+  end
+  
+  private
+  
+  def conversation_params
+    params.require(:conversation).permit(:title)
+  end
 end
