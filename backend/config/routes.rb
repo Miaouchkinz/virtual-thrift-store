11a0db5890d7028465c31c6f4d5f64e4f41c3dbd
Gab Richard <g.elodierichard@gmail.com>
2020-03-21 21:26:41 -0400

    add routing for conversations and messages

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,9 +6,15 @@ Rails.application.routes.draw do
     resources :users, :clothings, :bookmarks, :clothing_categories
   end
   
+  # Login/logout sessions routing
   resources :sessions, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
+
+  #Actioncable chat routing
+  resources :conversations, only: [:index, :create]
+  resources :messages, only: [:create]
+  mount Actioncable.server => '/cable'
 
   root to: "static#home"
 end
