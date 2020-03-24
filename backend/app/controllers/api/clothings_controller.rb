0cac8ae86a24200a3d75dee256f427d68f8e53e5
Gab Richard <g.elodierichard@gmail.com>
2020-03-24 14:40:14 -0400

    fix spacing

--- backend/app/controllers/api/clothings_controller.rb
+++ backend/app/controllers/api/clothings_controller.rb
@@ -1,8 +1,6 @@
 class Api::ClothingsController < ApplicationController
-
   def index
     clothings = Clothing.order(id: :desc).where(available_for_exchange: true)
     render json: clothings
   end
-
 end
