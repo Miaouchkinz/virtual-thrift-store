f923af7b896c92d5c674454351fd27f283c4c835
Gab Richard <g.elodierichard@gmail.com>
2020-03-24 18:12:05 -0400

    update to functional component and get user's name for each message sent

--- client/src/components/chat/MessageArea.jsx
+++ client/src/components/chat/MessageArea.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import NewMessageForm from './NewMessageForm';
 
-const MessagesArea = ({
-  conversation: { id, title, messages },
-}) => {
-  return (
-    <div className="messagesArea">
-      <h2>{title}</h2>
-      <ul>{orderedMessages(messages)}</ul>
-      <NewMessageForm conversation_id={id} />
-    </div>
-  );
-};
-
-export default MessagesArea;
+export default function MessagesArea({conversation: { id, title, messages }, currentUser, usersList}) {
+// UseEffect // TO REFACTOR IF TIME
+// get userId from conversations and find user info from /api/users, don't render all userslist in useApplicationData
 
 // helpers
 const orderedMessages = messages => {
   const sortedMessages = messages.sort(
     (a, b) => new Date(a.created_at) - new Date(b.created_at)
   );
-
   return sortedMessages.map(message => {
-    return <li key={message.id}>{message.text}</li>;
+    const user = usersList.find(user => user.id === message.user_id)
+
+    return <li key={message.id}>{user.name} said: {message.text}</li>;
   });
+};
+
+  return (
+    <div className="messagesArea">
+      <h2>{title}</h2>
+      <ul>{orderedMessages(messages)}</ul>
+      <NewMessageForm conversation_id={id} currentUser={currentUser} />
+    </div>
+  );
 };
