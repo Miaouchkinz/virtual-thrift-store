e4db77f740b038472ceaed1b17d123656e980e39
Gab Richard <g.elodierichard@gmail.com>
2020-03-24 20:46:05 -0400

    cleaned up comments

--- client/src/components/chat/NewConversationForm.jsx
+++ client/src/components/chat/NewConversationForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import axios from 'axios'
 import { API_ROOT, HEADERS } from '../../constants';
 
 export default function NewConversationForm({currentUser}){
