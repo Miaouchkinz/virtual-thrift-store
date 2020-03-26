f79036858dae175ec35396b32de71b691a34c762
Gab Richard <g.elodierichard@gmail.com>
2020-03-26 10:08:50 -0400

    update props and implement Moment to get time since last message

--- client/src/components/chat/ConvoPreview.jsx
+++ client/src/components/chat/ConvoPreview.jsx
@@ -1,6 +1,8 @@
-import react from "react";
+import React from "react";
+import Moment from "react-moment";
 
-export default function ConvoPreview() {
+export default function ConvoPreview({avatar, userName, conversation, latestMessageTime}) {
+
   return (
     <div className="chat-preview-container">
       <img
@@ -8,8 +10,14 @@ export default function ConvoPreview() {
         src={avatar}
         alt="chat-correspondent-avatar"
       />
-      <span className="chat-correspondent-name">OtherUser</span>
-      <span className="last-message-timestamp">Time since</span>
+      <span className="chat-correspondent-name">{userName}</span>
+      {latestMessageTime && 
+        <Moment 
+          className="last-message-timestamp" 
+          fromNow
+        >
+          {latestMessageTime}
+        </Moment>}
     </div>
   )
 }
