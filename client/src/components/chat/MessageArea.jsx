7ee9f047fc8e9eecd7a19557e2f05f2a6f1dae00
Gab Richard <g.elodierichard@gmail.com>
2020-03-25 20:52:40 -0400

    implement timestamp and pass in users for specific convo

--- client/src/components/chat/MessageArea.jsx
+++ client/src/components/chat/MessageArea.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import Moment from 'react-moment';
 import NewMessageForm from './NewMessageForm';
 
-export default function MessagesArea({conversation: { id, title, messages }, currentUser, usersList}) {
+export default function MessagesArea({conversation: { id, title, messages, user_1, user_2 }, currentUser}) {
 // UseEffect // TO REFACTOR IF TIME
 // get userId from conversations and find user info from /api/users, don't render all userslist in useApplicationData
 
-// helpers
 const orderedMessages = messages => {
   const sortedMessages = messages.sort(
     (a, b) => new Date(a.created_at) - new Date(b.created_at)
   );
   return sortedMessages.map(message => {
-    const user = usersList.find(user => user.id === message.user_id)
+    const user = message.user_id === currentUser.id ? user_1 : user_2
 
-    return <li key={message.id}>{user.name} said: {message.text}</li>;
+    return (
+      <div className="ind-message-container">
+        <li key={message.id}>
+          <img
+            className="message-area-avatar"
+            src={user.avatar_url}
+            alt="chat-correspondent-avatar"
+          />
+          {user.name} said: {message.text}
+        </li>
+        <Moment className="message-timestamp" fromNow>{message.created_at}</Moment>
+      </div>
+    );
   });
 };
 
@@ -21,7 +33,7 @@ const orderedMessages = messages => {
     <div className="messagesArea">
       <h2>{title}</h2>
       <ul>{orderedMessages(messages)}</ul>
-      <NewMessageForm conversation_id={id} currentUser={currentUser} />
+      <NewMessageForm conversation_id={id} currentUser_id={currentUser.id} />
     </div>
   );
 };
