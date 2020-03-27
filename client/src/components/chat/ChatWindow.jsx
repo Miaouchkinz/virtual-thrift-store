095d6cf26a9cf2f367481a42ee877fb128346e1e
Gab Richard <g.elodierichard@gmail.com>
2020-03-27 16:43:42 -0400

    implement chatWindow and render MessageArea component

--- client/src/components/chat/ChatWindow.jsx
+++ client/src/components/chat/ChatWindow.jsx
@@ -1,20 +1,51 @@
-import react from "react";
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import MessagesArea from "./MessageArea";
 import Icon from "../common/iconButton"
 
-export default function ChatWindow() {
+export default function ChatWindow({ conversations, currentUser }) {
+  function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
+  let query = useQuery();
+
+  const activeConversationID = Number(query.get('id'));
+
+  const findActiveConversation = (conversations, activeConversationID) => {
+    return conversations.find(
+      conversation => conversation.id === activeConversationID
+    );
+  };
+
+  const findCorrespondentUser = (conversations, activeConversationID) => {
+    const conversation = findActiveConversation(conversations, activeConversationID)
+    return conversation.user_2[0]
+  }
+
+  const correspondentUser = findCorrespondentUser(conversations, activeConversationID)
+
   return (
-    <div className="chat-window-header">
+    <div className="chat-window-header">   
       <nav>
-        <Icon secondary onClick={() => history.push('/user/profile')} label="<"></Icon>
-      </nav>
+        <Link to={'/user/profile'}>
+          <Icon secondary label="<"></Icon>
+        </Link>
+      </nav> 
       <header>
         <img
           className="chat-window-avatar"
-          src={avatar}
+          src={correspondentUser.avatar_url}
           alt="chat-correspondent-avatar"
         />
-        <span className="chat-correspondent-name">Correspondent's name</span>
+        <span className="chat-correspondent-name">{correspondentUser.name}</span>
       </header>
-    </div>
+        <MessagesArea
+          currentUser={currentUser}
+          conversation={findActiveConversation(
+            conversations,
+            activeConversationID
+          )}
+        />
+    </div> 
   )
 }
