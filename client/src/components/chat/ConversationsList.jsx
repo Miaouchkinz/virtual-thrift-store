5cc956a54260c998bf221e77239ec198bfda5ac3
Gab Richard <g.elodierichard@gmail.com>
2020-03-29 22:36:56 -0400

    add functionality to see last mess sent and time

--- client/src/components/chat/ConversationsList.jsx
+++ client/src/components/chat/ConversationsList.jsx
@@ -5,26 +5,21 @@ import ConvoPreview from './ConvoPreview';
 
 
 export default function ConversationsList({conversations, handleReceivedConversation, currentUserId}) {
-
   const mapConversations = (conversations) => {
     const orderedConversations = conversations.sort(
-        (a, b) => new Date(a.created_at) - new Date(b.created_at)
+        (a, b) => new Date(b.created_at) - new Date(a.created_at)
     );
-
-    const latestMessageTime = orderedConversations.map( (conversation) => {
-      if (conversation.messages.length) {
-        const lastMessage = conversation.messages[conversation.messages.length-1]
-        return lastMessage.created_at
-      }
-    });
 
     return orderedConversations.map((conversation, i) => {
       const correspondentUser = currentUserId === conversation.user_1[0].id ? conversation.user_2[0] : conversation.user_1[0];
+      const lastMessage = conversation.messages.length ? conversation.messages[conversation.messages.length-1].text : conversation.title;
+      const lastUpdated = conversation.messages.length ? conversation.messages[conversation.messages.length-1].created_at : conversation.created_at;
+      
       return (
         <div className="ind-conv-preview-container" key={conversation.id}>
           <li>
           <Link to={`/chat?id=${conversation.id}`}>
-            <ConvoPreview avatar={correspondentUser.avatar_url} userName={correspondentUser.name} conversation={conversation} latestMessageTime={latestMessageTime[i]}/>
+            <ConvoPreview avatar={correspondentUser.avatar_url} userName={correspondentUser.name} lastMessagePreview={lastMessage} latestMessageTime={lastUpdated}/>
           </Link>
 
           </li>
