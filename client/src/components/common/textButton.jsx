422710d2a52e174977a11f42b65494544ce00d2c
Gab Richard <g.elodierichard@gmail.com>
2020-03-29 22:38:00 -0400

    add small to props

--- client/src/components/common/textButton.jsx
+++ client/src/components/common/textButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function Button({ primary, secondary, fullWidth, onClick, label, type }) {
+export default function Button({ primary, secondary, small, fullWidth, onClick, label, type }) {
   let classList = [];
   if (primary) classList.push('primary');
   else if (secondary) classList.push('secondary');
+
+
+  if (small) classList.push("small");
 
   if (fullWidth) classList.push('full-width')
 
