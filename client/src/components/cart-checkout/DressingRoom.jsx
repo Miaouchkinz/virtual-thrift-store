e676494507a12e25d64326c9c7af6f0ccbf21f21
vagrant <frederique.bordeleau@gmail.com>
2020-03-30 04:29:52 +0000

    Able to open camera and console.log pose-posenet

--- client/src/components/cart-checkout/DressingRoom.jsx
+++ client/src/components/cart-checkout/DressingRoom.jsx
@@ -12,41 +12,51 @@ export default function DressingRoom({ itemData }) {
   }, [itemData.itemUrl]);
 
   let imageElement = React.createRef();
+  let videoElement = React.createRef();
+
+  const startWebcam = () => {
+    const constraints = { video: true };
+    const webcam = document.getElementById("user_camera");
+    navigator.mediaDevices.getUserMedia(constraints).then(video => {
+      webcam.srcObject = video;
+    });
+    return webcam;
+  };
 
   const checkLimage = () => {
     if (itemData.itemUrl !== null) {
       posenet
         .load()
         .then(function(net) {
-          const pose = net.estimateSinglePose(imageElement.current, {
-            flipHorizontal: false
-          });
+          const imageScaleFactor = 0.5;
+          const flipHorizontal = false;
+          const outputStride = 16;
+          const pose = net.estimateSinglePose(
+            videoElement.current,
+            imageScaleFactor,
+            flipHorizontal,
+            outputStride
+          );
           return pose;
         })
         .then(function(pose) {
+          console.log(pose);
           const leftShoulder = pose.keypoints[6].position.x;
           const rightShoulder = pose.keypoints[5].position.x;
-
           const leftOffsetPercent = itemData.itemLeft;
           const rightOffsetPercent = itemData.itemRight;
           const topOffsetPercent = itemData.itemTop;
-
           const selectedItem = document.getElementById("selected-item");
-
           const leftOffset = selectedItem.width * leftOffsetPercent;
           const rightOffset = selectedItem.width * rightOffsetPercent;
           const topOffset = selectedItem.height * topOffsetPercent;
-
           const leftPos = leftShoulder - leftOffset;
           const rightPos = rightShoulder + rightOffset;
           const width = rightPos - leftPos;
-
           const topLeftOfShoulder = pose.keypoints[6].position.y;
           const topRightOfShoulder = pose.keypoints[5].position.y;
-
           const topDistance =
             (topLeftOfShoulder + topRightOfShoulder) / 2 - topOffset;
-
           setWidth(`${width}px`);
           setLeft(`${leftPos}px`);
           setWidth(`${width}px`);
@@ -57,17 +67,24 @@ export default function DressingRoom({ itemData }) {
 
   return (
     <div className="images">
+      <button onClick={startWebcam}>OPEN CAMERA</button>
       <img
         onClick={checkLimage}
         id="selected-item"
         src={itemData.itemUrl}
         style={{ width, left, top }}
       />
-      <img
+      <video
+        ref={videoElement}
+        style={{ width: 800, height: 500, backgroundColor: "black" }}
+        autoPlay
+        id="user_camera"
+      ></video>
+      {/* <img
         ref={imageElement}
         id="person_standing"
         src="./images/person_1.jpg"
-      />
+      /> */}
     </div>
   );
 }
