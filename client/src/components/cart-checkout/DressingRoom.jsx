46680e14db420589ff18fc0d3225de114bd2b850
vagrant <frederique.bordeleau@gmail.com>
2020-03-31 03:13:50 +0000

    Fixed maths for posenet

--- client/src/components/cart-checkout/DressingRoom.jsx
+++ client/src/components/cart-checkout/DressingRoom.jsx
@@ -21,29 +21,49 @@ export default function DressingRoom({ selectedItemForTrying }) {
         .then(function(pose) {
           console.log(pose);
 
-          const leftShoulder = pose.keypoints[6].position.x;
-          const rightShoulder = pose.keypoints[5].position.x;
+          const leftShoulder = pose.keypoints[6].position.x; // 152
+          const rightShoulder = pose.keypoints[5].position.x; // 272
 
-          const leftOffsetPercent = Number(selectedItemForTrying.itemLeft);
-          const rightOffsetPercent = Number(selectedItemForTrying.itemRight);
-          const topOffsetPercent = Number(selectedItemForTrying.itemTop);
+          const leftOffsetPercent = Number(selectedItemForTrying.itemLeft); // 0.26
+          const rightOffsetPercent = Number(selectedItemForTrying.itemRight); // 0.29
+          const topOffsetPercent = Number(selectedItemForTrying.itemTop); // 0.14
 
-          const leftOffset = selectedItem.current.width * leftOffsetPercent;
-          const rightOffset = selectedItem.current.width * rightOffsetPercent;
-          const topOffset = selectedItem.current.height * topOffsetPercent;
+          const leftOffset = selectedItem.current.width * leftOffsetPercent; // 500 * 0.26 = 130 ??
+          const rightOffset = selectedItem.current.width * rightOffsetPercent; // 500 * 0.29 = 145 ??
+          const topOffset = selectedItem.current.height * topOffsetPercent; // 500 * 0.14 = 70
 
-          const leftPos = leftShoulder - leftOffset;
-          const rightPos = rightShoulder + rightOffset;
-          const width = rightPos - leftPos;
+          const spaceBetweenShouldersItem =
+            selectedItem.current.width - leftOffset - rightOffset; // 500 - 130 - 145 = 225
+          console.log(spaceBetweenShouldersItem);
+          const spaceBetweenShouldersModel = rightShoulder - leftShoulder; // 272 - 152 = 120
+          console.log(spaceBetweenShouldersModel);
+
+          const scalingRatio =
+            spaceBetweenShouldersItem / spaceBetweenShouldersModel; // 1.875
+
+          console.log(scalingRatio);
+          // const width = rightPos - leftPos; // 417 - 22 = 395
+          const width = selectedItem.current.width / scalingRatio; // 500 / 1.875 = 266.66px
+          console.log(width);
+          const distBetweenLeftAndLeftShoulderOfItem =
+            width * leftOffsetPercent; // 70px
+          console.log(distBetweenLeftAndLeftShoulderOfItem);
+          //This is what we call Apalachian Case
+
+          const leftOffsetOfItemOnModel =
+            leftShoulder - distBetweenLeftAndLeftShoulderOfItem;
+          console.log(leftOffsetOfItemOnModel);
+          // const leftPos = leftShoulder - leftOffset; // 152 - 130 = 22
+          // const rightPos = rightShoulder + rightOffset; // 272 + 145 = 417
 
           const topLeftOfShoulder = pose.keypoints[6].position.y;
           const topRightOfShoulder = pose.keypoints[5].position.y;
 
-          const topDistance =
-            (topLeftOfShoulder + topRightOfShoulder) / 2 - topOffset;
+          // const topDistance = (topLeftOfShoulder + topRightOfShoulder) / 2 - topOffset;
+          const topDistance = topLeftOfShoulder - width * topOffsetPercent;
 
           setWidth(`${width}px`);
-          setLeft(`${leftPos}px`);
+          setLeft(`${leftOffsetOfItemOnModel}px`);
           setTop(`${topDistance}px`);
         });
     }
